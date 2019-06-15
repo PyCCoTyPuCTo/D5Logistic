06bad7962ce3549c8605c2babe46531664f6daa1
AnatoliyShilov <anatolishil@gmail.com>
2019-06-15 20:44:08 +0300

    Добавлены таблицы: Склад-Товар, Товар-Заказ, Заказ, Магазин.
    Добавление внешних ключей.
    Таблицы: Склад-Товар, Товар-Заказ, Заказ , Магазин.

---
+++ database/migrations/2019_06_15_171328_create_products_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProductsOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('order_id');
+            $table->unsignedBigInteger('product_id');
+            $table->integer('count');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_orders');
+    }
+}
