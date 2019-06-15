72bc251a248a216ba5bc1e89f9cb67a5b8c35d4d
Kadin Maxim <m.kadin@crpo.su>
2019-06-15 20:39:32 +0300

    Drop storage_vehicle table

---
+++ database/migrations/2019_06_15_173807_drop_storage_vehicle_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class DropStorageVehicleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::drop('storage_vehicle');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('storage_vehicle', function (Blueprint $table) {
+            //
+        });
+    }
+}
