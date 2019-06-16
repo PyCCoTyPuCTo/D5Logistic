857680ec8014d895f4c9cbb61fb0a10c81aa87a0
BezlepkinAndrey <andbezlepkin@rambler.ru>
2019-06-16 16:14:59 +0300

    fsdff

---
+++ app/Action/Shop/UpdateAction.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: thisi
+ * Date: 15.06.2019
+ * Time: 23:23
+ */
+namespace App\Action\Shop;
+use App\Shop;
+class UpdateAction
+{
+    private $data;
+    private $id;
+    /**
+     * UpdateAction constructor.
+     * @param $data
+     */
+    public function __construct($data, $id)
+    {
+        $this->data = $data;
+        $this->id = $id;
+    }
+    public function update() {
+        $shop = Shop::find($this->id);
+        $shop->update($this->data);
+    }
+}
