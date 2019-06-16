39efe34d3d3d6e837737c411bbb255a1aa2d47a3
Nero30stm <iancha1997@inbox.ru>
2019-06-16 08:22:19 +0300

    CSs для регистрации

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
 @section('content')
     <div class="container" style="margin-top: 4%">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-10 justify-content-center">
                 <div class="card">
                     <div class="card-header">{{ __('Register') }}</div>
 
