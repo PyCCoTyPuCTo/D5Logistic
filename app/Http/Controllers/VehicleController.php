461ba41789300da10ddaa9f20f486cdf0a9ea67b
BezlepkinAndrey <andbezlepkin@rambler.ru>
2019-06-16 13:55:04 +0300

    Добавление геолокации

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -7,25 +7,43 @@ use App\Action\Vehicles\DestroyVehicleAction;
 use App\Action\Vehicles\MoveToStorageAction;
 use App\Action\Vehicles\UpdateVehicleAction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Storage;
 
 class VehicleController extends Controller
 {
-    public function create(Request $request) {
+
+    public function getVehicleList(Request $request)
+    {
+        $id = Auth::id();
+        $storages = Storage::all()->where('user_id', '=', 'id');
+
+        if ($storages == null) {
+            $storages = [];
+        }
+        return view('seller.cars', ['storages' => $storages]);
+    }
+
+    public function create(Request $request)
+    {
         $createVehicle = new CreateVehicleAction($request);
         $createVehicle->create();
     }
 
-    public function update(Request $request) {
+    public function update(Request $request)
+    {
         $updateVehicle = new UpdateVehicleAction($request);
         $updateVehicle->update();
     }
 
-    public function destroy($id) {
+    public function destroy($id)
+    {
         $destroyVehicle = new DestroyVehicleAction($id);
         $destroyVehicle->destroy();
     }
 
-    public function moveToStorage($idVehicle, $idStorage) {
+    public function moveToStorage($idVehicle, $idStorage)
+    {
         $moveVehicle = new MoveToStorageAction($idVehicle, $idStorage);
         $moveVehicle->move();
     }
